873d6bac4753bd2ae870a0250283d4fce6a92745
NokibulAlam <nokibul.alam02@gmail.com>
2022-10-30 14:44:30 +0600

    Product Search UI done

--- src/Core/ApiCore.js
+++ src/Core/ApiCore.js
@@ -1,32 +1,34 @@
+import queryString from 'query-string';
 const API = 'http://localhost:4000/api';
+
 
 
 // Get Products
 export const getProducts = (sortBy) => {
-    return fetch(`${API}/products?sortBy=${sortBy}&order=desc&limit=12`, {
-      method: "GET",
+  return fetch(`${API}/products?sortBy=${sortBy}&order=desc&limit=12`, {
+    method: "GET",
+  })
+    .then((data) => {
+      return data.json();
     })
-      .then((data) => {
-        return data.json();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 
-  // Get Single Product
-  export const getSingleProduct = (productId) => {
-    return fetch(`${API}/product/${productId}`, {
-      method: "GET",
+// Get Single Product
+export const getSingleProduct = (productId) => {
+  return fetch(`${API}/product/${productId}`, {
+    method: "GET",
+  })
+    .then((data) => {
+      return data.json();
     })
-      .then((data) => {
-        return data.json();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 
 // Get Related Products for Single Product Page
@@ -46,12 +48,27 @@ export const getRelatedProducts = (productId) => {
 // Get All Category
 export const getCategory = () => {
   return fetch(`${API}/categories`, {
-          method: "GET",
-      })
-      .then((res) => {
-          return res.json();
-      })
-      .catch((err) => {
-          return err.json();
-      });
+    method: "GET",
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .catch((err) => {
+      return err.json();
+    });
+};
+
+
+// Get Search Products
+export const getSearchedProducts = (params) => {
+  const query = queryString.stringify(params);
+  return fetch(`${API}/products/search?${query}`, {
+    method: "GET",
+  })
+    .then((data) => {
+      return data.json();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
--- src/Core/Search/Search.js
+++ src/Core/Search/Search.js
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 
 // API
-import { getCategory } from '../ApiCore';
+import { getCategory, getSearchedProducts } from '../ApiCore';
 
 //css
 import './Search.css';
+
+//components
+import Card from '../../Core/Card/Card';
 
 const Search = () => {
     //state
@@ -33,23 +36,73 @@ const Search = () => {
         loadCategory();
     }, []);
 
+    const searchHandler = (name) => (event) => {
+        setData({ ...data, [name]: event.target.value, searched: false });
+    }
+
+    //get Search Result
+    const searchData = () => {
+        // console.log(search, category);
+        if (search) {
+            getSearchedProducts({
+                search: search || undefined,
+                category: category || "All"
+            })
+                .then((products) => {
+                    if (products.error) console.log(products.error);
+                    setData({ ...data, result: products, searched: true });
+                    // console.log(products);
+                })
+        }
+    }
+
+    // form submit
+    const searchSubmit = (e) => {
+        e.preventDefault();
+        searchData();
+    }
+
+    const showSearhMSG = (searched, result) => {
+        if (searched && result.length > 0) {
+            return `Found ${result.length} Products`;
+        }
+        else if (searched && result.length <= 0) {
+            return `No Products Found!`;
+        }
+    };
+
+    const showSearchProduct = (productData = []) => {
+        return (
+            <Container>
+                <h3 className="mt-4 mb-4 ml-4 searchResult"> {showSearhMSG(searched, result)} </h3>
+                <Row>
+                    {productData.map((product, i) => (
+                        <Col md={3} key={i}>
+                            <Card product={product} />
+                        </Col>
+                    ))}
+                </Row>
+            </Container>
+        )
+    }
+
     const searchBarForm = () => {
         return (
-            <form>
+            <form onSubmit={searchSubmit}>
                 <span className='input-group-text'>
                     <div className='input-group input-group-lg'>
                         <div className='input-group-prepend'>
-                            <select className='btn mr-2'>
+                            <select className='btn mr-2' style={{ textAlign: "left" }} onChange={searchHandler("category")}>
                                 <option value="All">All</option>
                                 {categories.map((category, i) => (
                                     <option key={i} value={category._id}>{category.name}</option>
                                 ))}
                             </select>
                         </div>
-                        <input type="search" className='form-control' placeholder='Search Item...'/>
+                        <input type="search" className='form-control' placeholder='Search Item...' onChange={searchHandler("search")} />
                     </div>
                     <div className='btn input-group-append' style={{ border: "none" }}>
-                            <button className='input-group-text'>Search</button>
+                        <button className='input-group-text'>Search</button>
                     </div>
                 </span>
             </form>
@@ -58,7 +111,8 @@ const Search = () => {
 
     return (
         <div>
-            <div className='my-3'>{searchBarForm()}</div>
+            <div className='mb-3 mt-3'>{searchBarForm()}</div>
+            {showSearchProduct(result)}
         </div>
     );
 };
